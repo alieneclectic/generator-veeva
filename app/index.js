8a3676d8065fb6be6a49a6ca39324bc41e02b8d7
Jason English <Jason.English@mccann.com>
2014-08-11 09:14:55 -0400

    folder setup

---
+++ app/index.js
@@ -0,0 +1,209 @@
+/*global require, module */
+(function () {
+    'use strict';
+
+    var join = require('path').join,
+        yeoman = require('yeoman-generator'),
+        chalk = require('chalk');
+
+    module.exports = yeoman.generators.Base.extend({
+        constructor: function () {
+            yeoman.generators.Base.apply(this, arguments);
+
+            // setup the test-framework property, Gruntfile template will need this
+            this.option('test-framework', {
+                desc: 'Test framework to be invoked',
+                type: String,
+                defaults: 'mocha'
+            });
+            this.testFramework = this.options['test-framework'];
+
+            this.option('coffee', {
+                desc: 'Use CoffeeScript',
+                type: Boolean,
+                defaults: false
+            });
+            this.coffee = this.options.coffee;
+
+            this.pkg = require('../package.json');
+        },
+
+        askFor: function () {
+            var done = this.async();
+
+            // welcome message
+            if (!this.options['skip-welcome-message']) {
+                this.log(require('yosay')());
+                this.log(chalk.magenta(
+                    'Out of the box I include HTML5 Boilerplate, jQuery, and a Gruntfile.js to build your app.'
+                ));
+            }
+
+            var prompts = [{
+                name: 'appName',
+                message: 'What is your app\'s name ?',
+                default: this.appname
+            }, {
+                name: 'sitemap',
+                message: 'Pass in the sitemap as arguments'
+            }, {
+                type: 'checkbox',
+                name: 'features',
+                message: 'What more would you like?',
+                choices: [{
+                    name: 'Bootstrap',
+                    value: 'includeBootstrap',
+                    checked: true
+                }, {
+                    name: 'Sass',
+                    value: 'includeSass',
+                    checked: false
+                }, {
+                    name: 'Modernizr',
+                    value: 'includeModernizr',
+                    checked: false
+                }]
+            }, {
+                when: function (answers) {
+                    return answers.features.indexOf('includeSass') !== -1;
+                },
+                type: 'confirm',
+                name: 'libsass',
+                value: 'includeLibSass',
+                message: 'Would you like to use libsass? Read up more at \n' +
+                    chalk.green('https://github.com/andrew/node-sass#node-sass'),
+                default: false
+            }];
+
+            this.prompt(prompts, function (answers) {
+                var features = answers.features;
+
+                function hasFeature(feat) {
+                    return features && features.indexOf(feat) !== -1;
+                }
+
+                this.includeSass = hasFeature('includeSass');
+                this.includeBootstrap = hasFeature('includeBootstrap');
+                this.includeModernizr = hasFeature('includeModernizr');
+                this.sitemap = answers.sitemap;
+                
+                this.includeLibSass = answers.libsass;
+                this.includeRubySass = !answers.libsass;
+
+                done();
+            }.bind(this));
+        },
+
+        gruntfile: function () {
+            this.template('Gruntfile.js');
+        },
+
+        packageJSON: function () {
+            this.template('_package.json', 'package.json');
+        },
+
+        git: function () {
+            this.template('gitignore', '.gitignore');
+            this.copy('gitattributes', '.gitattributes');
+        },
+
+        bower: function () {
+            this.template('_bower.json', 'bower.json');
+        },
+
+        jshint: function () {
+            this.copy('jshintrc', '.jshintrc');
+        },
+
+        editorConfig: function () {
+            this.copy('editorconfig', '.editorconfig');
+        },
+
+        h5bp: function () {
+            this.directory('app');
+        },
+
+        mainStylesheet: function () {
+            var css = 'main.' + (this.includeSass ? 's' : '') + 'css';
+            this.template(css, 'app/_globals/styles/' + css);
+        },
+
+        writeIndex: function () {
+            this.indexFile = this.readFileAsString(join(this.sourceRoot(), 'index.html'));
+            this.indexFile = this.engine(this.indexFile, this);
+
+            // wire Bootstrap plugins
+            if (this.includeBootstrap) {
+                var bs = 'bower_components/bootstrap';
+                bs += this.includeSass ? '-sass-official/vendor/assets/javascripts/bootstrap/' : '/js/';
+
+                this.indexFile = this.appendFiles({
+                    html: this.indexFile,
+                    fileType: 'js',
+                    optimizedPath: 'scripts/plugins.js',
+                    sourceFileList: [
+                        bs + 'affix.js',
+                        bs + 'alert.js',
+                        bs + 'dropdown.js',
+                        bs + 'tooltip.js',
+                        bs + 'modal.js',
+                        bs + 'transition.js',
+                        bs + 'button.js',
+                        bs + 'popover.js',
+                        bs + 'carousel.js',
+                        bs + 'scrollspy.js',
+                        bs + 'collapse.js',
+                        bs + 'tab.js'
+                    ],
+                    searchPath: '.'
+                });
+            }
+
+            this.indexFile = this.appendFiles({
+                html: this.indexFile,
+                fileType: 'js',
+                optimizedPath: '_globals/scripts/main.js',
+                sourceFileList: ['_globals/scripts/main.js'],
+                searchPath: ['app/_globals', '.tmp']
+            });
+        },
+
+        app: function () {
+            this.mkdir('app/_globals');
+            this.mkdir('app/_globals/scripts');
+            this.mkdir('app/_globals/styles');
+            this.mkdir('app/_globals/images');
+            this.mkdir('app/_globals/fonts');
+
+            this.write('app/index.html', this.indexFile);
+
+            if (this.coffee) {
+                this.write(
+                    'app/_globals/scripts/main.coffee',
+                    'console.log "\'Allo from CoffeeScript!"'
+                );
+            } else {
+                this.write('app/_globals/scripts/main.js', 'console.log(\'\\\'Allo \\\'Allo!\');');
+            }
+        },
+
+        install: function () {
+            this.on('end', function () {
+                this.invoke(this.options['test-framework'], {
+                    options: {
+                        'skip-message': this.options['skip-install-message'],
+                        'skip-install': this.options['skip-install'],
+                        'coffee': this.options.coffee
+                    }
+                });
+
+                if (!this.options['skip-install']) {
+                    this.installDependencies({
+                        skipMessage: this.options['skip-install-message'],
+                        skipInstall: this.options['skip-install']
+                    });
+                }
+            });
+        }
+    });
+}());
